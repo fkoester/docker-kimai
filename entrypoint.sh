94cabcaf6a372fd169c5e0610d28c8f09fe22ed9
Ralf Herzog <ralf@rherzog.de>
2018-09-13 20:11:07 +0200

    v1.3.1: Updated kimai and ldap by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,7 @@ fi
 echo -n "=> Fixing permissions on autoconf.php and auth.php ... "
 
 # Permissions on autoconf.php
+touch /var/www/html/includes/autoconf.php
 chown www-data: /var/www/html/includes/autoconf.php
 chmod 750 /var/www/html/includes/autoconf.php
 
@@ -31,7 +32,7 @@ then
   # Match not found. This means we need to add the current selected authenticator
 
   # Fixme: It would be nice to comment the $authenticator line. Unfortunately gives me an "Device or resource busy" error.
-  # sed -i "s/\$authenticator/# \$authenticator/" /var/www/html/includes/autoconf.php
+#  sed -i "/\$authenticator.*;/d" /var/www/html/includes/autoconf.php
   echo "\$authenticator = '${KIMAI_AUTHENTICATOR}';" >> /var/www/html/includes/autoconf.php
 fi
 
